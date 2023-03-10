2cd9d44a61701d591443d1210ecdd21edb6e44bd
Camilo Restrepo <camilorg2017@gmail.com>
2023-03-09 21:56:54 -0500

    create kravata project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kravata';
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.http
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false'
+      )
+      .subscribe((data: any) => {
+        this.data = data;
+      });
+  }
 }
