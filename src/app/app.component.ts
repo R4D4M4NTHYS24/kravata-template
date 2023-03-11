69d4c4d78a3a19f5e19fcae4df0c1004de3e6d06
Camilo Restrepo <camilorg2017@gmail.com>
2023-03-10 21:18:34 -0500

    create interface for data types in API array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  fully_diluted_valuation?: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  market_cap_change_24h: number;
+  market_cap_change_percentage_24h: number;
+  circulating_supply: number;
+  total_supply?: number;
+  max_supply?: number;
+  ath: number;
+  ath_change_percentage: number;
+  ath_date: string;
+  atl: number;
+  atl_change_percentage: number;
+  atl_date: string;
+  roi?: {
+    times: number;
+    currency: string;
+    percentage: number;
+  };
+  last_updated: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  data: any[] = [];
+export class AppComponent implements OnInit {
+  data: Coin[] = [];
 
   constructor(private http: HttpClient) {}
   ngOnInit() {
     this.http
-      .get(
+      .get<Coin[]>(
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1&sparkline=false'
       )
-      .subscribe((data: any) => {
+      .subscribe((data: Coin[]) => {
         this.data = data;
       });
   }
